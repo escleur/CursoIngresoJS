4accbab1505416119408eb9d058f0cb23977bc54
escleur <escleur@users.noreply.github.com>
2019-07-10 22:18:06 -0300

    Clase 4

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota;
+
+	nota = Math.floor(Math.random() * 10) + 1;
+
+	if(nota > 8){
+		alert("EXCELENTE");
+	}else if(nota > 4){
+		alert("APROBO");
+	}else{
+		alert("Vamos, la proxima se puede.");
+	}
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,7 +1,15 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad;
+    var estadoCivil;
 
+    edad = document.getElementById("edad").value;
+    estadoCivil = document.getElementById("estadoCivil").value;
+
+    if(edad < 18 && estadoCivil != "Soltero"){
+        alert("Es muy chico para no ser soltero");
+    }
 	
 
 
