57a1df9745687b796d381ce76e8593e1dac3990c
escleur <escleur@users.noreply.github.com>
2019-08-01 01:08:48 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -5,23 +5,72 @@ function mostrar()
     var temperatura;
     var respuesta;
 
+    var contadorTempPares = 0;
+    var pesoMaximo = 0;
+    var pesoMinimo = 101;
+    var marcaPesoMaximo;
+    var contadorTempCero = 0;
+    var contador = 0;
+    var pesoTotal = 0;
+    var promedio;
+
     do{
         marca = prompt("Ingrese la marca");
-        do{
-            peso = prompt("Ingrese el peso");
+
+        peso = prompt("Ingrese el peso de 1 a 100");
+        peso = parseInt(peso);
+        
+        while(isNaN(peso) || peso < 1 || peso > 100  ){
+            peso = prompt("ERROR Ingrese el peso de 1 a 100");
             peso = parseInt(peso);
-        }while(isNaN(peso) || peso < 1 || peso > 100  )
-        do{
-            temperatura = prompt("Ingrese la temperatura");
+        }
+
+        temperatura = prompt("Ingrese la temperatura de -30 a 30");
+        temperatura = parseInt(temperatura);
+
+        while(isNaN(temperatura) || temperatura < -30 || temperatura > 30  ){
+            temperatura = prompt("ERROR Ingrese la temperatura de -30 a 30");
             temperatura = parseInt(temperatura);
-        }while(isNaN(temperatura) || temperatura < -30 || temperatura > 30  )
+        }
+
+        if(temperatura%2 == 0){
+            contadorTempPares++;
+        }
+
+        if(peso > pesoMaximo){
+            pesoMaximo = peso;
+            marcaPesoMaximo = marca;
+        }
+
+        if(temperatura < 0){
+            contadorTempCero++;
+        }
+
+        contador++;
+        pesoTotal += peso;
+
+        if(peso < pesoMinimo){
+            pesoMinimo = peso;
+        }
+
+        respuesta = prompt("Ingrese n para terminar, s para continuar");
+
+        while( respuesta != "n" && respuesta != "s" ){
+            respuesta = prompt("ERROR Ingrese n para terminar, s para continuar");
+        }
+
+    }while(respuesta != "n");
+
+    promedio = pesoTotal / contador;
+
+    document.write("Temperaturas pares " + contadorTempPares + "<br>");
+    document.write("Marca producto mas pesado " + marcaPesoMaximo + "<br>");
+    document.write("Productos a menos de cero grados " + contadorTempCero + "<br>");
+    document.write("Promedio de pesos " + promedio + "<br>");
+    document.write("Peso maximo " + pesoMaximo + " y peso minimo" + pesoMinimo + "<br>");
 
 
-        do{
-            temperatura = prompt("Ingrese la temperatura");
-            temperatura = parseInt(temperatura);
-        }while( respuesta != "n" && respuesta != "s" )
 
-    }
+
 
 }
