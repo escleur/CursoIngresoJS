d56213093e0180f33997ef83c5648036ca9c975e
escleur <escleur@users.noreply.github.com>
2019-07-31 22:02:30 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,27 @@
 function mostrar()
 {
+    var marca;
+    var peso;
+    var temperatura;
+    var respuesta;
+
+    do{
+        marca = prompt("Ingrese la marca");
+        do{
+            peso = prompt("Ingrese el peso");
+            peso = parseInt(peso);
+        }while(isNaN(peso) || peso < 1 || peso > 100  )
+        do{
+            temperatura = prompt("Ingrese la temperatura");
+            temperatura = parseInt(temperatura);
+        }while(isNaN(temperatura) || temperatura < -30 || temperatura > 30  )
+
+
+        do{
+            temperatura = prompt("Ingrese la temperatura");
+            temperatura = parseInt(temperatura);
+        }while( respuesta != "n" && respuesta != "s" )
+
+    }
 
 }
