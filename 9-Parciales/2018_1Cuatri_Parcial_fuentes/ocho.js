c32c11c8decb36c91789d1ecc9dcaab7e28790b8
escleur <escleur@users.noreply.github.com>
2019-08-02 22:05:52 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,38 @@
+/*Bienvenidos. 
+Realizar el algoritmo que permita iterar el ingreso de dos datos,
+ una letra y un número entre -100 y 100 (validar) hasta que el usuario quiera e
+  informar al terminar el ingreso por document.write: 
+a) La cantidad de números pares. 
+b) La cantidad de números impares. 
+c) La cantidad de ceros. 
+d) El promedio de todos los números positivos ingresados. 
+e) La suma de todos los números negativos. 
+f) El número y la letra del máximo y el mínimo.
+*/
+
+var respuesta;
+
 function mostrar()
 {
 
+    var letra;
+    var numero;
+    do{
+
+        letra = prompt("Ingrese una letra");
+        while(letra.length != 1 || !isNaN(letra)){
+            letra = prompt("ERROR Ingrese una letra");
+        }
+        numero = prompt("Ingrese un numero entre -100 y 100");
+        numero = parseInt(numero);
+        while(isNaN(numero) || numero < -100 || numero > 100){
+            numero = prompt("ERROR Ingrese un numero entre -100 y 100");
+        }
+
+
+        respuesta = prompt("Ingrese s para continuar y n para salir")
+        while(respuesta != "s" && respuesta != "n"){
+            respuesta = prompt("Error Ingrese s para continuar y n para salir")
+        }
+    }while(respuesta == "s");
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,51 @@
+﻿/*Bienvenidos. 
+Realizar el algoritmo que permita el ingreso por prompt de las notas (validar entre 0 y 10) ,
+ el sexo (validar el sexo “f” o “m”) de 5 alumnos, informar por alert: 
+a) El promedio de las notas totales. 
+b) La nota más baja y el sexo de esa persona. 
+c) La cantidad de varones que su nota haya sido mayor o igual a 6.
+*/
+
 function mostrar()
 {
+    var nota;
+    var sexo;
+    var sumaTotal = 0;
+    var minimoNota;
+    var minimoSexo;
+    var varonesMayor5 = 0;
+    var promedio;
 
+    for(var i = 0; i < 5; i++){
+        nota = prompt("Ingrese una nota de 0 a 10");
+        nota = parseInt(nota);
+        while(isNaN(nota) || nota < 0 || nota > 10){
+            nota = prompt("ERROR Ingrese una nota de 0 a 10");
+            nota = parseInt(nota);
+        }
+
+        sexo = prompt("Ingrese el sexo m o f");
+        while(sexo != "m" && sexo != "f"){
+            sexo = prompt("ERROR Ingrese el sexo m o f");
+        }
+
+        sumaTotal += nota;
+        if(i == 0){
+            minimoNota = nota;
+            minimoSexo = sexo;
+        }else if(nota < minimoNota){
+            minimoNota = nota;
+            minimoSexo = sexo;
+        }
+        if(sexo == "m" && nota > 5){
+            varonesMayor5++;
+        }
+
+
+    }
+
+    promedio = sumaTotal / 5;
+    alert("El promedio de notas es " + promedio);
+    alert("Nota mas baja " + minimoNota + " y el sexo es " + minimoSexo);
+    alert("Cantidad de varones que su nota haya sido mayor o igual a 6 : " + varonesMayor5);
 }
